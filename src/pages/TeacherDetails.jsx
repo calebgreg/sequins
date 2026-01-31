59f075acf87d03f81f50659c25a19885e382aba5
cramer.calebcm <cramer.calebcm@gmail.com>
2026-01-31 18:48:38 +0000

    File changes

--- src/pages/TeacherDetails.jsx
+++ src/pages/TeacherDetails.jsx
@@ -490,6 +490,8 @@ const TeacherDetails = () => {
                     backgroundClip: 'text',
                     WebkitBackgroundClip: 'text',
                     color: 'transparent',
+                    textShadow: '0 2px 3px rgba(255,255,255,0.7), 0 -1px 1px rgba(120,80,80,0.15)',
+                    filter: 'drop-shadow(0 1px 0 rgba(255,255,255,0.5))',
                   }}
                 >
                   Approve Timecard
