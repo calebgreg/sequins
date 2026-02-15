83263d8d7764f6057df357617ccf5ec3d1775ca6
cramer.calebcm <cramer.calebcm@gmail.com>
2026-02-15 20:17:14 +0000

    File changes

--- src/pages/ClassManager.jsx
+++ src/pages/ClassManager.jsx
@@ -4,6 +4,7 @@ import { useQuery } from '@tanstack/react-query';
 import { base44 } from "@/api/base44Client";
 import { createPageUrl } from '../utils';
 import { ArrowLeft } from 'lucide-react';
+import UserMenu from '../components/layout/UserMenu';
 
 import ImportScheduleModal from '../components/manager/ImportScheduleModal';
 import StudentRecommender from '../components/manager/StudentRecommender';
@@ -109,6 +110,7 @@ export default function ClassManager() {
             </button>
           </div>
         </div>
+        <UserMenu />
       </div>
 
       {displayMode === 'now' ? (
